9497bc5dc933016b5887267db9a98418d114c3f4
Josh Morrow <josh@jcmorrow.com>
2015-02-12 01:06:09 -0800

    Cleaning up cruft

--- pareto_solution_by_josh.rb
+++ pareto_solution_by_josh.rb
@@ -1,18 +1,16 @@
-#first, some pseudo code!
+# Similar to the way VBA is being used in the Excel version of this report,
+# I'm going to go from CSV to CSV, and also include some terminal output 
+# for debugging and such.
 
-#Similar to the way VBA is being used in the Excel version of this report,
-#I'm going to go from CSV to CSV, and also include some terminal output 
-#for debugging and such.
+# In an ideal world, this would all be taking place on a server inside of a
+# web framework like rails, but for an intro to ruby, this should work just fine.
 
-#In an ideal world, this would all be taking place on a server inside of a
-#web framework like rails, but for an intro to ruby, this should work just fine.
-
-#to run this program, open up a terminal, and execute using:
+# to run this program, open up a terminal, and execute using:
 
 # ruby pareto_solution_by_josh.rb
 
-
-#important caveat: This program assumes that there is a file in the same directory called data.csv
+# important caveat: This program assumes that there is a file in the same directory called data.csv
+# which has the headers "id" and "amount" and "date"
 startTime = Time.now
 require 'CSV'
 
@@ -21,7 +19,9 @@ csv_text = File.read('data.csv')
 csv = CSV.parse(csv_text, :headers => true)
 year2 = 2014
 
-#we're going to build out from this object now so it is easier.
+# we're going to build out from this object now so it is easier
+# to access all of our hash values! I'm initializing the values
+# so I can get them straight in my head, it's not necessary though.
 giving_levels = {
 	"under100" => {"donor_count" => 0, "dollar_total" => 0, "percentage_total" => 0.0, "percentage_donors" => 0.0},
 	"100to249" => {"donor_count" => 0, "dollar_total" => 0, "percentage_total" => 0.0, "percentage_donors" => 0.0},
@@ -67,23 +67,14 @@ giving_levels.each do |level, stats|
 	stats["percentage_donors"] = (stats["donor_count"].to_f/grand_donor_count.to_f*100.0).round(2)
 end
 
-puts "Total donors: #{donor_totals.size}"
-giving_levels.each {|level, stats| puts "#{level} has #{stats["donor_count"]} donors in it and a total of $#{stats["dollar_total"].round(2)} in contributions, accounting for %#{stats["percentage_total"]} of the total gifts"}
-finishTime=Time.now
-puts "Total operation took #{finishTime - startTime} seconds"
-
-
-#=============================================================
-
-# ON TO STAGE 2
-
-#=============================================================
-
+#puts "Total donors: #{donor_totals.size}"
+#giving_levels.each {|level, stats| puts "#{level} has #{stats["donor_count"]} donors in it and a total of $#{stats["dollar_total"].round(2)} in contributions, accounting for %#{stats["percentage_total"]} of the total gifts"}
 pareto_amount = giving_levels["250to1k"]["percentage_total"] + giving_levels["1kto5k"]["percentage_total"]+giving_levels["above5k"]["percentage_total"]
 pareto_donors = giving_levels["250to1k"]["percentage_donors"] + giving_levels["1kto5k"]["percentage_donors"]+giving_levels["above5k"]["percentage_donors"]
 puts "%#{pareto_amount} of gifts were given by %#{pareto_donors.round(2)} of donors"
 
-
+finishTime=Time.now
+puts "Total operation took #{finishTime - startTime} seconds"
 
 
 
